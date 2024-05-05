c0f9988c5af5647fa11dc57f23c938e56790863d
Carciax <122102594+Carciax@users.noreply.github.com>
2024-05-06 00:01:00 +0200

    upgraded constants

--- Icarus-Verliog-testing/Paralellizer/paralellizer.sv
+++ Icarus-Verliog-testing/Paralellizer/paralellizer.sv
@@ -17,22 +17,46 @@ module Paralellizer (
     //Program
     input wire prog,
     //To encrypters
-    output reg [`NUM_ENCRYPTERS-1:0] encrypters_data [`ENCRYPTER_WIDTH-1:0],
-    output reg [`NUM_ENCRYPTERS-1] encrypters_key_rotation [`KEY_ROTATION_WIDTH-1:0],
+    output reg [`ENCRYPTER_WIDTH-1:0] encrypters_data  [`NUM_ENCRYPTERS-1:0],
+    output reg [`KEY_ROTATION_WIDTH-1:0] encrypters_key_rotation [`NUM_ENCRYPTERS-1],
     output reg [`NUM_ENCRYPTERS-1:0] encrypters_program,
     output reg [`NUM_ENCRYPTERS-1:0] encrypters_data_ready,
-    input wire [`NUM_ENCRYPTERS-1:0] encrypters_ready
+    input wire [`NUM_ENCRYPTERS-1:0] encrypters_ready,
+    //Watchers
+    output reg [2:0] state_out,
+    output reg [`KEY_WIDTH-1:0] key_out,
+    output reg [`KEY_ROTATION_WIDTH-1:0] key_rotation_out,
+    output reg [`KEY_QSPI_COUNT_REG-1:0] key_index_out,
+    output reg [`KEY_ENCRYPTER_COUNT_REG-1:0] key_encrypter_index_out,
+    output reg [`ENCRYPTER_WIDTH-1:0] encrypter_data_packet_out,
+    output reg [`ENCRYPTER_QSPI_COUNT_REG-1:0] encrypter_data_index_out,
+    output reg [`NUM_ENCRYPTERS_REG-1:0] encrypter_index_out
 );
 
 reg [2:0] state = `STATE_IDLE;
 reg [`KEY_WIDTH-1:0] key;
-reg [`KEY_ROTATION_WIDTH-1:0] key_rotation;
 reg [`KEY_QSPI_COUNT_REG-1:0] key_index;
+reg [`KEY_ROTATION_WIDTH-1:0] key_rotation;
+reg [`KEY_ROTATION_WIDTH_COUNT_REG-1:0] key_rotation_index;
+
+reg [`KEY_ENCRYPTER_COUNT_REG-1:0] key_encrypter_index;
+
 reg [`ENCRYPTER_WIDTH-1:0] encrypter_data_packet;
+reg [`ENCRYPTER_WIDTH_COUNT_REG-1:0] encrypter_data_subindex;
+
 reg [`ENCRYPTER_QSPI_COUNT_REG-1:0] encrypter_data_index;
 reg [`NUM_ENCRYPTERS_REG-1:0] encrypter_index;
+integer i;
 
 initial begin
+    assign state_out = state;
+    assign key_out = key;
+    assign key_rotation_out = key_rotation;
+    assign key_index_out = key_index;
+    assign encrypter_data_packet_out = encrypter_data_packet;
+    assign encrypter_data_index_out = encrypter_data_index;
+    assign encrypter_index_out = encrypter_index;
+
     qspi_ready = 1'b0;
     encrypters_program = 0;
     key = 0;
@@ -59,6 +83,7 @@ always @(posedge qspi_sending) begin
             state <= `STATE_ENCRYPTING;
             encrypter_index = 0;
             encrypter_data_index = 0;
+            encrypters_data_ready = 0;
             key_rotation = 0;
         end
 
@@ -89,6 +114,7 @@ always @(posedge clk) begin
             key_index = key_index + 1;
             if (key_index == `KEY_QSPI_COUNT) begin
                 state = `STATE_PROGRAMMING_ENCRYPTERS;
+                key_encrypter_index = 0;
                 qspi_ready = 1'b0;
             end
         end
@@ -103,6 +129,14 @@ always @(posedge clk) begin
             if (state == `STATE_IDLE) qspi_ready = 1'b1;
         end
 
+        `STATE_ENCRYPTING: begin
+            encrypter_data_packet[encrypter_data_index*4] = qspi_data[0];
+            encrypter_data_packet[encrypter_data_index*4+1] = qspi_data[1];
+            encrypter_data_packet[encrypter_data_index*4+2] = qspi_data[2];
+            encrypter_data_packet[encrypter_data_index*4+3] = qspi_data[3];
+            encrypter_data_index = encrypter_data_index + 1;
+        end
+
         default:;
 
     endcase
@@ -113,24 +147,30 @@ always @(negedge clk) begin
     case (state)
 
         `STATE_PROGRAMMING_ENCRYPTERS: begin
-            for (encrypter_index = 0; encrypter_index < `NUM_ENCRYPTERS; encrypter_index++) begin
-                encrypters_data[encrypter_index] = key;
+            for (encrypter_index = 0; encrypter_index < `NUM_ENCRYPTERS; encrypter_index = encrypter_index + 1) begin
+                for (encrypter_data_subindex = 0; encrypter_data_subindex < `ENCRYPTER_WIDTH; encrypter_data_subindex = encrypter_data_subindex + 1) begin
+                    encrypters_data[encrypter_index][encrypter_data_subindex] = key[key_encrypter_index * `ENCRYPTER_WIDTH + encrypter_data_subindex];
+                end
                 encrypters_program[encrypter_index] = 1'b1;
             end
-            state = `STATE_WAITING_FOR_READY_ENCRYPTERS;
+
+            key_encrypter_index = key_encrypter_index + 1;
+            if (key_encrypter_index == `KEY_ENCRYPTER_COUNT) state = `STATE_WAITING_FOR_READY_ENCRYPTERS;
         end
 
         `STATE_ENCRYPTING: begin
-            encrypter_data_packet[encrypter_data_index*4] = qspi_data[0];
-            encrypter_data_packet[encrypter_data_index*4+1] = qspi_data[1];
-            encrypter_data_packet[encrypter_data_index*4+2] = qspi_data[2];
-            encrypter_data_packet[encrypter_data_index*4+3] = qspi_data[3];
-            encrypter_data_index = encrypter_data_index + 1;
-
             if (encrypter_data_index == `ENCRYPTER_QSPI_COUNT) begin
                 encrypter_data_index = 0;
-                encrypters_data[encrypter_index] = encrypter_data_packet;
-                encrypters_key_rotation[encrypter_index] = key_rotation;
+                
+                for (encrypter_data_subindex = 0; encrypter_data_subindex < `ENCRYPTER_WIDTH; encrypter_data_subindex = encrypter_data_subindex + 1) begin
+                    encrypters_data[encrypter_index][encrypter_data_subindex] = encrypter_data_packet[encrypter_data_subindex];
+                end
+
+                for (key_rotation_index = 0; key_rotation_index < `KEY_ROTATION_WIDTH; key_rotation_index = key_rotation_index + 1) begin
+                    encrypters_key_rotation[encrypter_index][key_rotation_index] = key_rotation[key_rotation_index];
+                end
+
+                encrypters_data_ready[encrypter_index] = 1'b1;
 
                 encrypter_index = encrypter_index + 1;
                 if (encrypter_index == `NUM_ENCRYPTERS) encrypter_data_index = 0;
--- Icarus-Verliog-testing/Paralellizer/paralellizer_tb.sv
+++ Icarus-Verliog-testing/Paralellizer/paralellizer_tb.sv
@@ -1,35 +1,119 @@
 `timescale 1ns / 1ns
 `include "./Constants/constants.vh"
-`include "./Paralellizer/paralellizer.v"
+`include "./Paralellizer/paralellizer.sv"
 
 module Paralellizer_tb;
 
-    reg clk;
+    reg clk; 
     reg reset;
-
-    // QSPI data and program signals
+    //QSPI
     reg [3:0] qspi_data;
-    reg prog;
-
-    // Encrypter ready signals (mocked for testbench)
-    reg [`NUM_ENCRYPTERS-1:0] encrypters_ready;
-
-    // Output signals for monitoring
-    wire [`NUM_ENCRYPTERS-1:0] encrypters_data [`ENCRYPTER_WIDTH-1:0];
-    wire [`NUM_ENCRYPTERS-1:0] encrypters_key_rotation [`ENCRYPTER_WIDTH-1:0];
-    wire [`NUM_ENCRYPTERS-1:0] encrypters_program;
+    reg qspi_sending;
     wire qspi_ready;
+    //Program
+    reg prog;
+    //To encrypters
+    wire[`ENCRYPTER_WIDTH-1:0] encrypters_data [`NUM_ENCRYPTERS-1:0];
+    wire [`KEY_ROTATION_WIDTH-1:0] encrypters_key_rotation [`NUM_ENCRYPTERS-1];
+    wire [`NUM_ENCRYPTERS-1:0] encrypters_program;
+    wire [`NUM_ENCRYPTERS-1:0] encrypters_data_ready;
+    reg [`NUM_ENCRYPTERS-1:0] encrypters_ready;
+    //Watchers
+    wire [2:0] state_out;
+    wire [`KEY_WIDTH-1:0] key_out;
+    wire [`KEY_ROTATION_WIDTH-1:0] key_rotation_out;
+    wire [`KEY_QSPI_COUNT_REG-1:0] key_index_out;
+    wire [`KEY_ENCRYPTER_COUNT_REG-1:0] key_encrypter_index_out;
+    wire [`ENCRYPTER_WIDTH-1:0] encrypter_data_packet_out;
+    wire [`ENCRYPTER_QSPI_COUNT_REG-1:0] encrypter_data_index_out;
+    wire [`NUM_ENCRYPTERS_REG-1:0] encrypter_index_out;
+    reg [`ENCRYPTER_WIDTH-1:0] encrypters_data_slice;
 
     Paralellizer uut(
         .clk(clk),
         .reset(reset),
         .qspi_data(qspi_data),
-        .prog(prog),
+        .qspi_sending(qspi_sending),
         .qspi_ready(qspi_ready),
+        .prog(prog),
         .encrypters_data(encrypters_data),
         .encrypters_key_rotation(encrypters_key_rotation),
         .encrypters_program(encrypters_program),
-        .encrypters_ready(encrypters_ready)
+        .encrypters_data_ready(encrypters_data_ready),
+        .encrypters_ready(encrypters_ready),
+        .state_out(state_out),
+        .key_out(key_out),
+        .key_rotation_out(key_rotation_out),
+        .key_index_out(key_index_out),
+        .key_encrypter_index_out(key_encrypter_index_out),
+        .encrypter_data_packet_out(encrypter_data_packet_out),
+        .encrypter_data_index_out(encrypter_data_index_out),
+        .encrypter_index_out(encrypter_index_out)
     );
+    
+    reg [3:0] spi_data_values[15:0];
+    integer fd, i, j;
+    initial begin
+        fd = $fopen("./Constants/qspi_data.csv", "r");
+        if (fd == -1) begin
+            $display("Error opening file!");
+            $finish;  // Exit simulation on error
+        end
+        for (i = 0; i < 256; i++) begin
+            j = $fscanf(fd, "%h", spi_data_values[i]);
+        end
+        $fclose(fd);
+        $display(spi_data_values[0]);
+    end
+
+    initial begin
+        $dumpfile("Paralellizer/paralellizer_tb.vcd");
+        $dumpvars(0, Paralellizer_tb);
+        $display("Starting paralellizer testbench");
+        clk = 0;
+        reset = 0;
+        qspi_data = 0;
+        qspi_sending = 0;
+        prog = 0;
+        encrypters_ready = 0;
+
+
+        //expected use
+        //set data on neg edge, read on the rising
+        #2 reset = 1; #2 reset = 0;
+        #2 prog = 1; #2 prog = 0;
+        //send key
+        #2 qspi_sending = 1;
+        for (i = 0; i < `KEY_QSPI_COUNT; i = i + 1) begin
+            qspi_data = spi_data_values[i];
+            $display("Setting qspi_data to 0x%h", spi_data_values[i]);
+            #2;
+        end
+        qspi_sending = 0;
+
+        #10;
+        //simulate encrypters receiving key after paralellizer send it out
+        for (i = 0; i < `NUM_ENCRYPTERS; i = i + 1) begin
+            encrypters_ready[i] = 1;
+            #1;
+        end
+
+        //start encrypting data. can only be started from idle state
+        #4 qspi_sending = 1;
+        for (i = `KEY_QSPI_COUNT; i < 32; i = i + 1) begin
+            qspi_data = spi_data_values[i];
+            $display("Setting qspi_data to 0x%h", spi_data_values[i]);
+            #2;
+        end
+        #2 qspi_sending = 0;
+        #10
+        $display("Ending paralellizer testbench");
+        $finish;
+    end
+
+    always begin
+        #1 clk = ~clk;
+        encrypters_data_slice[`ENCRYPTER_WIDTH-1:0] = encrypters_data[1][`ENCRYPTER_WIDTH-1:0];
+    end
 
 endmodule
