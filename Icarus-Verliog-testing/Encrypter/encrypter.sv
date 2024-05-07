47862fc1b06c08c38340657c11e74b6ec40ab9ac
Si Teng Wu <wxxsit001@myuct.ac.za>
2024-05-07 12:39:11 +0200

    fix: encrypter

--- Icarus-Verliog-testing/Encrypter/encrypter.sv
+++ Icarus-Verliog-testing/Encrypter/encrypter.sv
@@ -3,9 +3,8 @@
 `define IDLE 3'b000
 `define READING_KEY 3'b001
 `define WAITING_RDY_DATA_IN 3'b010
-`define READING_DATA 3'b011
-`define ENCRYPTING 3'b100
-`define SENDING_DATA 3'b101
+`define READING_DATA_ENCRYPTING 3'b011
+`define SENDING_DATA 3'b100
 
 module Encrypter (
     input wire clk,
@@ -13,40 +12,41 @@ module Encrypter (
 
     input wire [`ENCRYPTER_WIDTH-1:0] dataIn,
     input wire [`KEY_ROTATION_WIDTH-1:0] rot_offset,
-    input wire rdyIn,
+    input wire dataRdyIn,
     input wire cap,
     input wire prog,
 
     output reg [`ENCRYPTER_WIDTH-1:0] dataOut,
-    output reg reqIn,
-    output reg reqOut,
+    output reg rdyIn,
+    output reg dataRdyOut,
 
     output reg [2:0] state,
-    output reg [`KEY_WIDTH-1:0] key,
+    output reg [`KEY_WIDTH-1:0] keyRotated,
     output reg [`ENCRYPTER_WIDTH-1:0] data_in
 );
 
-    reg [`ENCRYPTER_WIDTH-1:0] data_out;
-    reg [`KEY_WIDTH-1:0] keyTemp;
+    reg [`KEY_WIDTH-1:0] keyOrigin;
     reg [`KEY_ROTATION_WIDTH:0] offset;
 
     initial begin
         state <= `IDLE;
-        reqIn = 0;
-        reqOut = 0;
+        rdyIn = 0;
+        dataRdyOut = 0;
         dataOut = 0;
         data_in = 0;
-        key = 0;
+        keyOrigin = 0;
+        keyRotated = 0;
         offset = 0;
     end
 
     always @(posedge reset) begin
         state <= `IDLE;
-        reqIn = 0;
-        reqOut = 0;
+        rdyIn = 0;
+        dataRdyOut = 0;
         dataOut = 0;
         data_in = 0;
-        key = 0;
+        keyOrigin = 0;
+        keyRotated = 0;
         offset = 0;
     end
 
@@ -54,34 +54,30 @@ module Encrypter (
         state <= `READING_KEY;
     end
 
-    always @(posedge rdyIn) begin
-        state <= `READING_DATA;
+    always @(posedge dataRdyIn) begin
+        state <= `READING_DATA_ENCRYPTING;
     end
 
     always @(posedge cap) begin
         state <= `WAITING_RDY_DATA_IN;
-        reqOut = 0;
-        reqIn = 1;
+        dataRdyOut = 0;
+        rdyIn = 1;
     end
 
     always @(posedge clk) begin
         case (state)
             `READING_KEY: begin
-                keyTemp = dataIn;
+                keyOrigin = dataIn;
                 state = `WAITING_RDY_DATA_IN;
-                reqIn = 1;
+                rdyIn = 1;
             end
 
-            `READING_DATA: begin
+            `READING_DATA_ENCRYPTING: begin
                 data_in = dataIn;
                 offset = rot_offset;
-                state = `ENCRYPTING;
-                reqIn = 0;
-            end
-
-            `ENCRYPTING: begin
-                key = (keyTemp << offset) | (keyTemp >> (`KEY_WIDTH-offset));
-                data_out = data_in ^ key;
+                rdyIn = 0;
+                keyRotated = (keyOrigin << offset) | (keyOrigin >> (`KEY_WIDTH-offset));
+                dataOut = data_in ^ keyRotated;
                 state = `SENDING_DATA;
             end
         endcase
@@ -90,8 +86,7 @@ module Encrypter (
     always @(negedge clk) begin
         case (state)
             `SENDING_DATA: begin
-                dataOut = data_out;
-                reqOut = 1;
+                dataRdyOut = 1;
             end
         endcase
     end
