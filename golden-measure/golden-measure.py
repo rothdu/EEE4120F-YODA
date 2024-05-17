491040d6f64b8f0c1c8226b369cfa7f61d8a0c92
rothdu <rtdugmore@gmail.com>
2024-05-17 21:53:19 +0200

    Added complex encryption mode to golden measure

--- golden-measure/golden-measure.py
+++ golden-measure/golden-measure.py
@@ -10,21 +10,30 @@ import os
 nBits = 32
 dataPath = "../data/rotating-xor.csv"
 key = 0xB4352B93
+truncator32 = 0xFFFFFFFF
+truncator5 = 0b11111
+complexEncrypt = True
 
-def main():
-    # used for truncating values to nBits length
-    truncator = np.power(2, nBits) - 1
+def main(): 
     
     # read CSV and truncate all data values to 32 bits exactly
     csvPath = os.fsencode(dataPath)
     df = pd.read_csv(os.fsdecode(csvPath), converters={'INPUT': lambda x: int(x, 16)})
-    df["INPUT"] = np.bitwise_and(df["INPUT"], truncator) 
+    df["INPUT"] = np.bitwise_and(df["INPUT"], truncator32) 
     
     # Generate arr of shifted keys
-    keyArr = np.full(len(df["INPUT"]), np.bitwise_and(key, truncator))
-    shifts = np.mod(np.arange(len(keyArr)), nBits)
-    keyArr = np.bitwise_or(np.bitwise_and(np.left_shift(keyArr, shifts), truncator), \
-    np.right_shift(keyArr, nBits - shifts))
+    keyArr = np.full(len(df["INPUT"]), np.bitwise_and(key, truncator32)) # create array of keys and ensure all are no longer than 32 bits
+    shifts = np.mod(np.arange(len(keyArr)), nBits) # create array of shift amounts (mod32 to repeat every 32)
+    if (complexEncrypt):
+        shifts = np.bitwise_xor(shifts, np.bitwise_and(truncator5))
+
+    keyArr = np.bitwise_or(np.bitwise_and(np.left_shift(keyArr, shifts), truncator32), \
+    np.right_shift(keyArr, nBits - shifts)) # perform rotation on keys by shift amount
+    
+    if complexEncrypt:
+        keyArr = np.power(keyArr, 2) # square
+        keyArr = np.mod(keyArr, 4294967311) # mod the big prime number
+        keyArr = np.bitwise_and(keyArr, truncator32) # truncate back down to 32 bits
     
     # XOR each data piece with relevant key data
     df["GOLDEN"] = np.bitwise_xor(df["INPUT"], keyArr)
